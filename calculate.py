8a605d87959aa89036dea25f649987cab8a3c7ab
jinhuo <506215537@qq.com>
2018-09-24 22:07:31 +0800

    panj's code

--- calculate.py
+++ calculate.py
@@ -1,3 +1,12 @@
+def is_leep(year):
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 date = input("请输入一个日期(yyyy-mm-dd)：")
 s = date.split('-')
@@ -6,7 +15,6 @@ if len(s) != 3:
 for i in s:
     if not is_number(i):
         exit('非法输入')
-print(s)
 year = int(s[0])
 month = int(s[1])
 day = int(s[2])
@@ -20,38 +28,18 @@ elif month in [1,3,5,7,8,10,12]:
 elif month in [4,6,9,11]:
     if day > 30 or day <= 0:
         exit('天数有误')
-elif month in [2]:
-    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+elif month == 2:
+    if is_leep(year):
         if day > 29 or day <= 0:
             exit('天数有误')
     else:
         if day > 28 or day <= 0:
             exit('天数有误')
-print(year)
-print(month)
-print(day)
-daylist = [31,29,31,30,31,30,31,31,30,31,30,31]
-n = 0
+daylist = [31,28,31,30,31,30,31,31,30,31,30,31]
 sum = 0
-if is_leep(year):
-    #print('%04d是闰年，2月份有29天。'%year)
-    for n in range(month - 1):
-        sum += daylist[n]
-    #print(sum + day)
-else:
-    #print('%04d不是闰年，2月份有28天。'%year)
-    daylist[1] = 28
-    for n in range(month - 1):
-        sum += daylist[n]
-    #print(sum + day)
-
-
-def is_leep(year):
-    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
-
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+for n in range(month-1):
+    if is_leep(year): 
+        daylist[1] = 29
+    sum += daylist[n]
+sum += day
+print(sum)
