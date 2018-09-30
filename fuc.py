f7a6a0cdbe88c127fbcfa35d1919022f5e96da64
jinhuo <506215537@qq.com>
2018-09-30 22:41:10 +0800

    commet

--- fuc.py
+++ fuc.py
@@ -1,6 +1,8 @@
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+words = ['cat','window','defenetrate']
+for w in words:
+    print(w,len(w))
+for w in words:
+    if len(w) > 6 :
+        words.insert(0,w)
+exit when len(words) > 6 :
+print (words)
